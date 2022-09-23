fa07b46de8664610037eea0e6cd2842ccf7308f9
Mattsptc <mateusptc@gmail.com>
2022-09-23 16:49:09 -0300

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,16 @@
 
+function updateProfileInfo(profileData) {
+    const photo = document.querySelector('#profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+    const name = document.querySelector('#profile.name')
+    const job = document.querySelector('#profile.job')
+    const location = document.querySelector('#profile.location')
+    const phone = document.querySelector('#profile.phone')
+    const email = document.querySelector('#profile.email')
+
+}
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
-    const profileName = await profile.name
-    document.innerText.name = profileName
-    console.log(profileName)
+    updateProfileInfo(profileData)
 })()
